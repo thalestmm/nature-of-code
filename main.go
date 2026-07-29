0516181f5a8b404e6d168f0118a9da4ea875215c
Thales Meier <meier.thales@gmail.com>
2026-07-29 17:35:25 -0300

    feat: rendering scattering square

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -18,6 +19,18 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	backgroundColor := color.RGBA{241, 143, 1, 255}
 	screen.Fill(backgroundColor)
+
+	// Render a simple square
+	entityColor := color.RGBA{0, 0, 0, 255}
+	square := ebiten.NewImage(10, 10)
+	square.Fill(entityColor)
+
+	op := &ebiten.DrawImageOptions{}
+	rx := rand.Float64()
+	ry := rand.Float64()
+	op.GeoM.Translate(rx*800.0, ry*600.0)
+
+	screen.DrawImage(square, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
