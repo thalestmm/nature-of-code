09badf71703868b3e1b4372d00f08986947d943e
Thales Meier <meier.thales@gmail.com>
2026-07-29 17:13:23 -0300

    chore: base screen rendering

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// Game implements the ebiten.Game interface
+type Game struct {
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	backgroundColor := color.RGBA{241, 143, 1, 255}
+	screen.Fill(backgroundColor)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	game := &Game{}
+
+	ebiten.SetWindowSize(800, 600)
+	ebiten.SetWindowTitle("Nature of Code")
+
+	log.Fatal(ebiten.RunGame(game))
+}
